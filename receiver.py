aa375ceb7b3fded609634087215901e119b54834
Rolando Fernandez <rolando@ebi.ac.uk>
2018-03-21 16:17:08 +0000

    added completeBundle() to send a message about completed assay bundles

--- receiver.py
+++ receiver.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import pika
+import json
 from ingestbroker.broker.ingestexportservice import IngestExporter
 
 DEFAULT_RABBIT_URL=os.environ.get('RABBIT_URL', 'amqp://localhost:5672')
@@ -8,6 +10,8 @@ LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 LOGGER = logging.getLogger(__name__)
 
+EXCHANGE = 'ingest.assays.exchange'
+ASSAY_COMPLETED_ROUTING_KEY = 'ingest.assays.completed'
 class IngestReceiver:
 
     def __init__(self):
@@ -17,6 +21,23 @@ class IngestReceiver:
         try:
             ingestExporter = IngestExporter()
             ingestExporter.generateAssayBundle(newAssayMessage)
-            ingestExporter.completeBundle(newAssayMessage)
+            self.completeBundle(newAssayMessage)
         except Exception, e:
             self.logger.exception("Failed to export to dss: "+newAssayMessage["callbackLink"]+ ", error:"+str(e))
+
+    def completeBundle(self, assayMessage):
+        self.logger.info("Sending a completed message for assay "+assayMessage["callbackLink"])
+
+        assayCompletedMessage = dict()
+
+        assayCompletedMessage["documentId"] = assayMessage["documentId"]
+        assayCompletedMessage["envelopeUuid"] = assayMessage["envelopeUuid"]
+        assayCompletedMessage["assayIndex"] = assayMessage["assayIndex"]
+        assayCompletedMessage["totalAssays"] = assayMessage["totalAssays"]
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(DEFAULT_RABBIT_URL))
+        channel = connection.channel()
+        channel.basic_publish(exchange=EXCHANGE,
+                              routing_key=ASSAY_COMPLETED_ROUTING_KEY,
+                              body=json.dumps(assayCompletedMessage))
+        connection.close()
