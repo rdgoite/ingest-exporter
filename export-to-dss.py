dee9b6b815852e608072ad1c5866134954d996ed
Alegria Aclan <aaclan@ebi.ac.uk>
2018-03-29 11:51:44 +0100

    more robust consumer

--- export-to-dss.py
+++ export-to-dss.py
@@ -6,65 +6,44 @@ call the ingest export service to generate the bundles and submit bundles to dat
 __author__ = "jupp"
 __license__ = "Apache 2.0"
 
-
 from optparse import OptionParser
 import os, sys, json
-import time
 import logging
-from kombu import Connection, Exchange, Queue, Producer
+from kombu import Connection, Exchange, Queue
+from kombu.mixins import ConsumerMixin
+
 from receiver import IngestReceiver
 
-
-DEFAULT_RABBIT_URL=os.path.expandvars(os.environ.get('RABBIT_URL', 'amqp://localhost:5672'))
+DEFAULT_RABBIT_URL = os.path.expandvars(os.environ.get('RABBIT_URL', 'amqp://localhost:5672'))
 EXCHANGE = 'ingest.assays.exchange'
 EXCHANGE_TYPE = 'topic'
 QUEUE = 'ingest.assays.bundle.create'
 ROUTING_KEY = 'ingest.assays.submitted'
 
+logger = logging.getLogger(__name__)
+receiver = IngestReceiver()
 
 
-def initReceivers(options):
-    logger = logging.getLogger(__name__)
+class Worker(ConsumerMixin):
+    def __init__(self, connection, queues):
+        self.connection = connection
+        self.queues = queues
 
-    receiver = IngestReceiver()
+    def get_consumers(self, Consumer, channel):
+        return [Consumer(queues=self.queues,
+                         callbacks=[self.on_message])]
 
-    def requeue(body):
-        with Connection(DEFAULT_RABBIT_URL) as publish_conn:
-            with publish_conn.channel() as channel:
-                producer = Producer(channel)
-                producer.publish(
-                    body,
-                    retry=True,
-                    exchange=EXCHANGE,
-                    routing_key=ROUTING_KEY
-                )
-
-    def callback(body, message):
+    def on_message(self, body, message):
         success = False
         try:
             receiver.run(json.loads(body))
             success = True
         except Exception, e:
             logger.exception(str(e))
-            requeue(body)
-            logger.info('Requeueing due to error! ' + str(message.delivery_tag))
 
         if success:
+            message.ack()
             logger.info('Finished! ' + str(message.delivery_tag))
-
-
-
-    assayExchange = Exchange(EXCHANGE, EXCHANGE_TYPE, passive=True, durable=False)
-    assayCreatedQueue = Queue(QUEUE, exchange=assayExchange, routing_key=ROUTING_KEY, durable=False, no_ack=True)
-
-
-    with Connection(DEFAULT_RABBIT_URL, connect_timeout=1000, heartbeat=1000) as conn:
-        # consume
-        with conn.Consumer(assayCreatedQueue, callbacks=[callback]) as consumer:
-            # Process messages and handle events on all channels
-            while True:
-                conn.drain_events()
-                conn.heartbeat_check()
 
 
 if __name__ == '__main__':
@@ -77,4 +56,10 @@ if __name__ == '__main__':
     parser.add_option("-l", "--log", help="the logging level", default='INFO')
 
     (options, args) = parser.parse_args()
-    initReceivers(options)
+
+    assay_exchange = Exchange(EXCHANGE, type=EXCHANGE_TYPE)
+    assay_queues = [Queue(QUEUE, assay_exchange, routing_key=ROUTING_KEY)]
+
+    with Connection(DEFAULT_RABBIT_URL, heartbeat=4) as conn:
+        worker = Worker(conn, assay_queues)
+        worker.run()
